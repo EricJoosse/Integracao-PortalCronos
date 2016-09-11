8504f49267bf3b290aab46a91f6867865f93b278
Eric <Eric@172.31.12.155>
2016-09-11 15:15:29 -0300

    Documentação caminho arquivo .properties em todos os tipos de ambientes.

--- src/pcronos/integracao/fornecedor/IntegracaoFornecedorCompleta.java
+++ src/pcronos/integracao/fornecedor/IntegracaoFornecedorCompleta.java
@@ -68,13 +68,20 @@ import java.security.AccessController;
 
 final class IntegracaoFornecedorCompleta {
   
-						     // Relative paths (those without a leading '/') mean that the resource 
-						     // will be searched relative to the directory which represents the package 
-						     // the class is in. 
-                         	// (Quer dizer: in a stand-alone environment.  "Run" in Eclipse searches however relative to the project root). 
-						     // Using an absolute path (one that starts with '/') means that the current 
-						     // package is ignored.
-  public static final String NOME_ARQUIVO_PROPERTIES = "conf/Integração Fornecedor - Portal Cronos.properties";  
+   public static final String NOME_ARQUIVO_PROPERTIES = "conf/Integração Fornecedor - Portal Cronos.properties";
+                              // Using an absolute path (one that starts WITH '/') means that the current 
+                              // package is ignored.
+
+	                         //  Relative paths (those WITHOUT a leading '/') mean that the resource 
+						     //  will be searched relative to the directory which represents the package 
+						     //  the class is in. 
+                         	//  Quer dizer: in a stand-alone environment (production env. por exemplo, sem Eclipse).  
+	                        //  O comando "Run" in Eclipse searches however relative to the project root, and not relative to /src, 
+	                        //  and DOS also searches however relative to the current directory DOS is in.
+	                        //  No ambiiente de produção o Agendador de Tarefas de Windows executa um arquivo .bat 
+	                        //  então também searches relative to the current directory DOS is in, which happens to coincide 
+	                        //  with the directory the jar-file is in.	
+  
   public static final String NAO_OFERTADA_IMPACTO_SE_ALTERAR = "não ofertada";
   public static Locale       locale;
   public static NumberFormat nf;
