e578c5983c2aa388726096ed2a079257eb2c69e1
eric <eric@Desenv01.v5e55e1oujdu5csnym2xtqjvra.bx.internal.cloudapp.net>
2018-11-30 15:03:16 -0300

    Parametrização da versão do Integrador - Parte 2 de 2

--- Instalador/Windows/Arquivos de Programas PC/Integração Fornecedor - Portal Cronos/bin/DesinstalarManualTI.bat
+++ Instalador/Windows/Arquivos de Programas PC/Integração Fornecedor - Portal Cronos/bin/DesinstalarManualTI.bat
@@ -7,6 +7,13 @@ call (exit /b 0)
 REM Limpar CLASSPATH :
 REM set CLASSPATH=
 
+
+REM By default, variables are global to your entire command prompt session. 
+REM Call the SETLOCAL command to make variables local to the scope of your script. 
+REM After calling SETLOCAL, any variable assignments revert upon calling ENDLOCAL, 
+REM calling EXIT, or when execution reaches the end of file (EOF) in your script.
+REM The setlocal command sets the ERRORLEVEL variable to 1.
+REM "SETLOCAL" é necessário aqui para poder usar ERRORLEVEL corretamente:   
 SETLOCAL
 
 call Versao.bat
