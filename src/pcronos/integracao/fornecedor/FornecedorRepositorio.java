936e6c60cbe0c478f300f7a00d8a2d51e02f5e50
eric <eric@Desenv01.v5e55e1oujdu5csnym2xtqjvra.bx.internal.cloudapp.net>
2018-12-19 08:40:18 -0300

    Normalização do FornecedorRepositorio - getFornecedor()

--- src/pcronos/integracao/fornecedor/FornecedorRepositorio.java
+++ src/pcronos/integracao/fornecedor/FornecedorRepositorio.java
@@ -23,56 +23,173 @@ public class FornecedorRepositorio {
 		f1.IdFornecedor = 2016;
 		f1.NomeFantasiaEmpresa = "Teste Windows Server 2016";
 		f1.versaoIntegrador = "";
+		f1.SiglaSistemaFornecedor = "WinThor";
+		f1.EmailResponsavelTI = "";
+		f1.EmailResponsavelTIAlternativo = "";
+		f1.ApelidoResponsavelTI = "";
+		f1.ApelidoResponsavelTIAlternativo = "";
+		f1.AplicativoDesktopRemoto = "";
+		f1.usuarioWebservice = "";
+		f1.versaoJRE = "";
+		f1.tipoSO = "Windows Server 2016";
+		f1.dirProgramFiles = "Program Files";
 		hashMap.put(1, f1);
 
 		f2.IdFornecedor = null;
 		f2.NomeFantasiaEmpresa = "Monitoramento";
 		f2.versaoIntegrador = "2.8.4";
+		f2.SiglaSistemaFornecedor = "PCronos";
+		f2.EmailResponsavelTI = "";
+		f2.EmailResponsavelTIAlternativo = "";
+		f2.ApelidoResponsavelTI = "";
+		f2.ApelidoResponsavelTIAlternativo = "";
+		f2.AplicativoDesktopRemoto = "mstsc";
+		f2.versaoJRE = "jre1.8.0_92";
+		f2.tipoSO = "Windows Server 2008 R2 SP1";
+		f2.dirProgramFiles = "Program Files";
 		hashMap.put(2, f2);
 
 		f3.IdFornecedor = 13;
 		f3.NomeFantasiaEmpresa = "Formaggio";
 		f3.versaoIntegrador = "2.6";
+		f3.SiglaSistemaFornecedor = "APS";
+		f3.EmailResponsavelTI = "projetosti@formaggio.com.br";  
+		f3.EmailResponsavelTIAlternativo = "ti@formaggio.com.br??????"; 
+		f3.ApelidoResponsavelTI = "Geymison";
+		f3.ApelidoResponsavelTIAlternativo = "";
+		f3.FuncaoResponsavelTI = "Coordenador de Projetos de TI";
+		f3.FuncaoResponsavelTIAlternativo = "";
+		f3.AplicativoDesktopRemoto = "Team Viewer";
+		f3.usuarioWebservice = "ws-formaggio";
+		f3.versaoJRE = "jre1.8.0_92";
+		f3.tipoSO = "Windows Server 2012 R2";
+		f3.dirProgramFiles = "Program Files";
 		hashMap.put(3, f3);
 
 		f4.IdFornecedor = 947;
 		f4.NomeFantasiaEmpresa = "JR Distribuição";
 		f4.versaoIntegrador = "2.8.1";
+		f4.SiglaSistemaFornecedor = "WinThor";
+		f4.EmailResponsavelTI = "jrembalagem.ti@gmail.com"; 
+		f4.EmailResponsavelTIAlternativo = "";
+		f4.ApelidoResponsavelTI = "Ivan";
+		f4.ApelidoResponsavelTIAlternativo = "";
+		f4.AplicativoDesktopRemoto = "AnyDesk";
+		f4.IdAplicativoDesktopRemoto = "734228115";
+		f4.usuarioWebservice = "ws-jrembalagem";
+		f4.versaoJRE = "jre1.8.0_92";
+		f4.tipoSO = "Windows 10 Pro";
+		f4.SO32ou64bit = "64bit";
+		f4.dirProgramFiles = "Program Files";
 		hashMap.put(4, f4);
 
 		f5.IdFornecedor = 30;
 		f5.NomeFantasiaEmpresa = "Prolac";
 		f5.versaoIntegrador = "2.8.2";
+		f5.SiglaSistemaFornecedor = "WinThor";
+		f5.EmailResponsavelTI = "mscomprolac@gmail.com"; 
+		f5.EmailResponsavelTIAlternativo = "marcelo@casadoqueijo.net.br";
+		f5.ApelidoResponsavelTI = "Marcos";
+		f5.ApelidoResponsavelTIAlternativo = "Marcelo";
+		f5.AplicativoDesktopRemoto = "AnyDesk";
+		f5.IdAplicativoDesktopRemoto = "425769045";
+		f5.usuarioWebservice = "ws-prolac";
+		f5.versaoJRE = "jre1.8.0_191";
+		f5.tipoSO = "Windows Server 2008 R2 SP1";
+		f5.dirProgramFiles = "Program Files (x86)";
 		hashMap.put(5, f5);
 
 		f6.IdFornecedor = 21;
 		f6.NomeFantasiaEmpresa = "Marítimos";
 		f6.versaoIntegrador = "1.2.3";
+		f6.SiglaSistemaFornecedor = "WinThor";
+		f6.EmailResponsavelTI = "felipe.lolaia@maritimospescados.com.br"; 
+		f6.EmailResponsavelTIAlternativo = "";
+		f6.ApelidoResponsavelTI = "Felipe";
+		f6.ApelidoResponsavelTIAlternativo = "";
+		f6.AplicativoDesktopRemoto = "Team Viewer";
+		f6.usuarioWebservice = "ws-mpescados";
+		f6.versaoJRE = "jre1.8.0_92";
+		f6.tipoSO = "?????????????????????";
+		f6.dirProgramFiles = "Program Files";
 		hashMap.put(6, f6);
 
 		f7.IdFornecedor = 170;
 		f7.NomeFantasiaEmpresa = "SOST";
 		f7.versaoIntegrador = "2.3.1";
+		f7.SiglaSistemaFornecedor = "WinThor";
+		f7.EmailResponsavelTI = "informatica@sost.com.br OU cleijonatassilva@sost.com.br"; 
+		f7.EmailResponsavelTIAlternativo = "informatica@sost.com.br OU carlossena@sost.com.br";
+		f7.ApelidoResponsavelTI = "Cleijonatas";
+		f7.ApelidoResponsavelTIAlternativo = "Sena";
+		f7.AplicativoDesktopRemoto = "AnyDesk";
+		f7.usuarioWebservice = "ws-sost";
+		f7.versaoJRE = "jre1.8.0_92";
+		f7.tipoSO = "Windows Server 2008 R2 SP1";
+		f7.dirProgramFiles = "Program Files";
 		hashMap.put(7, f7);
 
 		f8.IdFornecedor = 60;
 		f8.NomeFantasiaEmpresa = "Karne Keijo";
 		f8.versaoIntegrador = "2.4.1";
+		f8.SiglaSistemaFornecedor = "SAP";
+		f8.EmailResponsavelTI = "timons@kk.com.br";
+		f8.EmailResponsavelTIAlternativo = "";
+		f8.ApelidoResponsavelTI = "Timon";
+		f8.ApelidoResponsavelTIAlternativo = "";
+		f8.AplicativoDesktopRemoto = "Team Viewer";
+		f8.usuarioWebservice = "ws-karnekeijo";
+		f8.versaoJRE = "jre1.8.0_92";
+		f8.tipoSO = "Windows Server 2008 R2 SP1";
+		f8.dirProgramFiles = "Program Files";
 		hashMap.put(8, f8);
 
 		f9.IdFornecedor = 33;
 		f9.NomeFantasiaEmpresa = "Comal";
 		f9.versaoIntegrador = "2.8.2";
+		f9.SiglaSistemaFornecedor = "WinThor";
+		f9.EmailResponsavelTI = "ti@comalcomercio.com.br";
+		f9.EmailResponsavelTIAlternativo = "";
+		f9.ApelidoResponsavelTI = "Rildson"; 
+						// Tem vontade para ajudar, porém é muito incompetente 
+						// e muito burro, tem que explicar tudo detalhadamente
+		f9.ApelidoResponsavelTIAlternativo = "";
+		f9.AplicativoDesktopRemoto = "AnyDesk";
+		f9.usuarioWebservice = "ws-comal";
+		f9.versaoJRE = "jre1.8.0_92";
+		f9.tipoSO = "Windows Server 2016"; // Windows Server 2016 Standard
+		f9.dirProgramFiles = "Program Files";
 		hashMap.put(9, f9);
 
 		f10.IdFornecedor = 171;
 		f10.NomeFantasiaEmpresa = "Propão";
 		f10.versaoIntegrador = "2.6.1";
+		f10.SiglaSistemaFornecedor = "WinThor";
+		f10.EmailResponsavelTI = "ti@propao.com.br";
+		f10.EmailResponsavelTIAlternativo = "";
+		f10.ApelidoResponsavelTI = "Elthon"; 
+						// Desenrolado, responsável para o cadastro de De-Para´s
+		f10.ApelidoResponsavelTIAlternativo = "";
+		f10.AplicativoDesktopRemoto = "AnyDesk";
+		f10.usuarioWebservice = "ws-propao";
+		f10.versaoJRE = "jre1.8.0_92";
+		f10.tipoSO = "Windows Server 2008 R2 SP1"; // Na verdade "Windows Server 2008 R2 Enterprise" sem nenhum service pack.........
+		f10.dirProgramFiles = "Program Files";
 		hashMap.put(10, f10);
 
 		f11.IdFornecedor = 14;
 		f11.NomeFantasiaEmpresa = "Padeirão";
 		f11.versaoIntegrador = "2.8.3";
+		f11.SiglaSistemaFornecedor = "WinThor";
+		f11.EmailResponsavelTI = "tiagoautran@padeirao.com";
+		f11.EmailResponsavelTIAlternativo = "";
+		f11.ApelidoResponsavelTI = "Tiago"; 
+		f11.ApelidoResponsavelTIAlternativo = "";
+		f11.AplicativoDesktopRemoto = "AnyDesk";
+		f11.usuarioWebservice = "ws-padeirao";
+		f11.versaoJRE = "jre1.8.0_111";
+		f11.tipoSO = "Windows Server 2012 R2"; // Windows Server 2012 R2 Standard (sem nenhum service pack)
+		f11.dirProgramFiles = "Program Files";
 		hashMap.put(11, f11);
 		
     }
@@ -112,175 +229,13 @@ public class FornecedorRepositorio {
 	}
 	
 	
-	Fornecedor getFornecedor(Integer idFornecedor) throws Exception {
-		Fornecedor f = new Fornecedor();
-		f.IdFornecedor = idFornecedor;
-
-		if (idFornecedor == 2016) {
-			f.NomeFantasiaEmpresa = "Teste Windows Server 2016";
-			f.versaoIntegrador = "";
-			f.SiglaSistemaFornecedor = "WinThor";
-			f.EmailResponsavelTI = "";
-			f.EmailResponsavelTIAlternativo = "";
-			f.ApelidoResponsavelTI = "";
-			f.ApelidoResponsavelTIAlternativo = "";
-			f.AplicativoDesktopRemoto = "";
-			f.usuarioWebservice = "";
-			f.versaoJRE = "";
-			f.tipoSO = "Windows Server 2016";
-			f.dirProgramFiles = "Program Files";
-		}			   
-		else if (idFornecedor == null) {
-			f.NomeFantasiaEmpresa = "Monitoramento";
-			f.versaoIntegrador = "2.8.4";
-			f.SiglaSistemaFornecedor = "PCronos";
-			f.EmailResponsavelTI = "";
-			f.EmailResponsavelTIAlternativo = "";
-			f.ApelidoResponsavelTI = "";
-			f.ApelidoResponsavelTIAlternativo = "";
-			f.AplicativoDesktopRemoto = "mstsc";
-			f.versaoJRE = "jre1.8.0_92";
-			f.tipoSO = "Windows Server 2008 R2 SP1";
-			f.dirProgramFiles = "Program Files";
-		}			   
-		else if (idFornecedor == 13) {
-			f.NomeFantasiaEmpresa = "Formaggio";
-			f.versaoIntegrador = "2.6";
-			f.SiglaSistemaFornecedor = "APS";
-			f.EmailResponsavelTI = "projetosti@formaggio.com.br";  
-			f.EmailResponsavelTIAlternativo = "ti@formaggio.com.br??????"; 
-			f.ApelidoResponsavelTI = "Geymison";
-			f.ApelidoResponsavelTIAlternativo = "";
-			f.FuncaoResponsavelTI = "Coordenador de Projetos de TI";
-			f.FuncaoResponsavelTIAlternativo = "";
-			f.AplicativoDesktopRemoto = "Team Viewer";
-			f.usuarioWebservice = "ws-formaggio";
-			f.versaoJRE = "jre1.8.0_92";
-			f.tipoSO = "Windows Server 2012 R2";
-			f.dirProgramFiles = "Program Files";
-		}			   
-		else if (idFornecedor == 947) {
-			f.NomeFantasiaEmpresa = "JR Distribuição";
-			f.versaoIntegrador = "2.8.1";
-			f.SiglaSistemaFornecedor = "WinThor";
-			f.EmailResponsavelTI = "jrembalagem.ti@gmail.com"; 
-			f.EmailResponsavelTIAlternativo = "";
-			f.ApelidoResponsavelTI = "Ivan";
-			f.ApelidoResponsavelTIAlternativo = "";
-			f.AplicativoDesktopRemoto = "AnyDesk";
-			f.IdAplicativoDesktopRemoto = "734228115";
-			f.usuarioWebservice = "ws-jrembalagem";
-			f.versaoJRE = "jre1.8.0_92";
-			f.tipoSO = "Windows 10 Pro";
-			f.SO32ou64bit = "64bit";
-			f.dirProgramFiles = "Program Files";
-		}			   
-		else if (idFornecedor == 30) {
-			f.NomeFantasiaEmpresa = "Prolac";
-			f.versaoIntegrador = "2.8.2";
-			f.SiglaSistemaFornecedor = "WinThor";
-			f.EmailResponsavelTI = "mscomprolac@gmail.com"; 
-			f.EmailResponsavelTIAlternativo = "marcelo@casadoqueijo.net.br";
-			f.ApelidoResponsavelTI = "Marcos";
-			f.ApelidoResponsavelTIAlternativo = "Marcelo";
-			f.AplicativoDesktopRemoto = "AnyDesk";
-			f.IdAplicativoDesktopRemoto = "425769045";
-			f.usuarioWebservice = "ws-prolac";
-			f.versaoJRE = "jre1.8.0_191";
-			f.tipoSO = "Windows Server 2008 R2 SP1";
-			f.dirProgramFiles = "Program Files (x86)";
-		}			   
-		else if (idFornecedor == 21) {
-			f.NomeFantasiaEmpresa = "Marítimos";
-			f.versaoIntegrador = "1.2.3";
-			f.SiglaSistemaFornecedor = "WinThor";
-			f.EmailResponsavelTI = "felipe.lolaia@maritimospescados.com.br"; 
-			f.EmailResponsavelTIAlternativo = "";
-			f.ApelidoResponsavelTI = "Felipe";
-			f.ApelidoResponsavelTIAlternativo = "";
-			f.AplicativoDesktopRemoto = "Team Viewer";
-			f.usuarioWebservice = "ws-mpescados";
-			f.versaoJRE = "jre1.8.0_92";
-			f.tipoSO = "?????????????????????";
-			f.dirProgramFiles = "Program Files";
-		}			   
-		else if (idFornecedor == 170) {
-			f.NomeFantasiaEmpresa = "SOST";
-			f.versaoIntegrador = "2.3.1";
-			f.SiglaSistemaFornecedor = "WinThor";
-			f.EmailResponsavelTI = "informatica@sost.com.br OU cleijonatassilva@sost.com.br"; 
-			f.EmailResponsavelTIAlternativo = "informatica@sost.com.br OU carlossena@sost.com.br";
-			f.ApelidoResponsavelTI = "Cleijonatas";
-			f.ApelidoResponsavelTIAlternativo = "Sena";
-			f.AplicativoDesktopRemoto = "AnyDesk";
-			f.usuarioWebservice = "ws-sost";
-			f.versaoJRE = "jre1.8.0_92";
-			f.tipoSO = "Windows Server 2008 R2 SP1";
-			f.dirProgramFiles = "Program Files";
-		}			   
-		else if (idFornecedor == 60) {
-			f.NomeFantasiaEmpresa = "Karne Keijo";
-			f.versaoIntegrador = "2.4.1";
-			f.SiglaSistemaFornecedor = "SAP";
-			f.EmailResponsavelTI = "timons@kk.com.br";
-			f.EmailResponsavelTIAlternativo = "";
-			f.ApelidoResponsavelTI = "Timon";
-			f.ApelidoResponsavelTIAlternativo = "";
-			f.AplicativoDesktopRemoto = "Team Viewer";
-			f.usuarioWebservice = "ws-karnekeijo";
-			f.versaoJRE = "jre1.8.0_92";
-			f.tipoSO = "Windows Server 2008 R2 SP1";
-			f.dirProgramFiles = "Program Files";
-		}			   
-		else if (idFornecedor == 33) {
-			f.NomeFantasiaEmpresa = "Comal";
-			f.versaoIntegrador = "2.8.2";
-			f.SiglaSistemaFornecedor = "WinThor";
-			f.EmailResponsavelTI = "ti@comalcomercio.com.br";
-			f.EmailResponsavelTIAlternativo = "";
-			f.ApelidoResponsavelTI = "Rildson"; 
-							// Tem vontade para ajudar, porém é muito incompetente 
-							// e muito burro, tem que explicar tudo detalhadamente
-			f.ApelidoResponsavelTIAlternativo = "";
-			f.AplicativoDesktopRemoto = "AnyDesk";
-			f.usuarioWebservice = "ws-comal";
-			f.versaoJRE = "jre1.8.0_92";
-			f.tipoSO = "Windows Server 2016"; // Windows Server 2016 Standard
-			f.dirProgramFiles = "Program Files";
-		}			   
-		else if (idFornecedor == 171) {
-			f.NomeFantasiaEmpresa = "Propão";
-			f.versaoIntegrador = "2.6.1";
-			f.SiglaSistemaFornecedor = "WinThor";
-			f.EmailResponsavelTI = "ti@propao.com.br";
-			f.EmailResponsavelTIAlternativo = "";
-			f.ApelidoResponsavelTI = "Elthon"; 
-							// Desenrolado, responsável para o cadastro de De-Para´s
-			f.ApelidoResponsavelTIAlternativo = "";
-			f.AplicativoDesktopRemoto = "AnyDesk";
-			f.usuarioWebservice = "ws-propao";
-			f.versaoJRE = "jre1.8.0_92";
-			f.tipoSO = "Windows Server 2008 R2 SP1"; // Na verdade "Windows Server 2008 R2 Enterprise" sem nenhum service pack.........
-			f.dirProgramFiles = "Program Files";
-		}			   
-		else if (idFornecedor == 14) {
-			f.NomeFantasiaEmpresa = "Padeirão";
-			f.versaoIntegrador = "2.8.3";
-			f.SiglaSistemaFornecedor = "WinThor";
-			f.EmailResponsavelTI = "tiagoautran@padeirao.com";
-			f.EmailResponsavelTIAlternativo = "";
-			f.ApelidoResponsavelTI = "Tiago"; 
-			f.ApelidoResponsavelTIAlternativo = "";
-			f.AplicativoDesktopRemoto = "AnyDesk";
-			f.usuarioWebservice = "ws-padeirao";
-			f.versaoJRE = "jre1.8.0_111";
-			f.tipoSO = "Windows Server 2012 R2"; // Windows Server 2012 R2 Standard (sem nenhum service pack)
-			f.dirProgramFiles = "Program Files";
-		}			   
-		else { 
-			throw new Exception("Erro: idFornecedor " + idFornecedor.toString() + " não existe");
-		}
+	Fornecedor getFornecedor(Integer idFornecedor) throws Exception {		
+		Fornecedor f = hashMap.get(idFornecedor);
 		
+		if (f == null)
+			throw new Exception("Erro: idFornecedor " + idFornecedor.toString() + " não existe");
+
 		return f;
 	}
+
 }
