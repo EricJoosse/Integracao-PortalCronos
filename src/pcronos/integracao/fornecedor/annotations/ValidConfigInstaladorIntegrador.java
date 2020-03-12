d5bf7d7b877971f6894d1b2efe67693780756c8a
eric <eric@Desenv01.v5e55e1oujdu5csnym2xtqjvra.bx.internal.cloudapp.net>
2020-03-12 08:44:58 -0300

    Novas custom annotation attributes

---
+++ src/pcronos/integracao/fornecedor/annotations/ValidConfigInstaladorIntegrador.java
@@ -0,0 +1,23 @@
+package pcronos.integracao.fornecedor.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import pcronos.integracao.fornecedor.validators.ConfigInstaladorIntegradorValidator;
+
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Constraint(validatedBy = ConfigInstaladorIntegradorValidator.class)
+public @interface ValidConfigInstaladorIntegrador 
+{
+   String message() default "Erro interno! Mensagem não definida!";
+   Class<?>[] groups() default { };
+   Class<? extends Payload>[] payload() default { };
+}
+
