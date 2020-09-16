fd3de3fb85c419b0d5591f80c97dc542168b48be
eric <eric@Desenv01.v5e55e1oujdu5csnym2xtqjvra.bx.internal.cloudapp.net>
2020-09-16 10:22:26 -0300

    Impactos upgrade Manual TI

--- src/pcronos/integracao/fornecedor/ManualManutencao.java
+++ src/pcronos/integracao/fornecedor/ManualManutencao.java
@@ -40,7 +40,7 @@ public class ManualManutencao {
 
 
 	private void setNomeArquivoMaisAtalho() throws Exception { 	
-		this.nomeArquivo = "Manual solucionamento paradas da integração Portal Cronos - v1.5.0 (20.09.2019).txt";
+		this.nomeArquivo = "Manual solucionamento paradas da integração Portal Cronos - v1.5.1 (13.09.2020).txt";
 		this.nomeAtalho = "Resolver Paradas.lnk";
 
 //		if (this.fornecedor.tipoSO.equals("Windows Server 2008 R2 SP1")) {
