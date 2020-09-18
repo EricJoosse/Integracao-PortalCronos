5a18c9732f3c93b6de598b95badd36f8f85f62a4
eric <eric@Desenv01.v5e55e1oujdu5csnym2xtqjvra.bx.internal.cloudapp.net>
2020-09-18 13:53:18 -0300

    Correções paths Testador Unitário

--- Instalador/Windows/Arquivos de Programas PC/Integração Fornecedor - Portal Cronos/bin/ComponenteTestador.bat
+++ Instalador/Windows/Arquivos de Programas PC/Integração Fornecedor - Portal Cronos/bin/ComponenteTestador.bat
@@ -37,19 +37,4 @@ if exist C:/PCronos/"Integração Fornecedor - Portal Cronos"/Instalador/Windows/"
 REM "pwd" em Linux = "%cd% em DOS:
 echo %cd%
 
-if "%1"=="Desinstalador.log" (
-  if exist Desinstalador.log del /f /q Desinstalador.log 
-) else if "%1"=="Instalador.log" (
-  if exist Instalador.log del /f /q Instalador.log 
-) else if "%1"=="TesteTresParam.log" (
-    echo MSGBOX "if TesteTresParam.log entrado corretamente!" > %temp%\TEMPmessage.vbs
-    call %temp%\TEMPmessage.vbs
-    del %temp%\TEMPmessage.vbs /f /q
-) else if "%1"=="TestadorUnitario.log" (
-  if exist TestadorUnitario.log del /f /q TestadorUnitario.log 
-)
-
-set path=C:\Program Files\Java\jre1.8.0_92\bin;%path%
-C:/"Program Files"/Java/jre1.8.0_92/bin/java.exe -cp integr-fornecedor-%versaoIntegrador%.jar pcronos.integracao.fornecedor.%2 %3 >> %1
-
-
+call CaminhoJRE.bat %1 %2 %3
