d605c5ac6f34ae5529e09fd50d5b57e1118d69d5
eric <eric@Desenv01.v5e55e1oujdu5csnym2xtqjvra.bx.internal.cloudapp.net>
2020-10-08 09:38:54 -0300

    Cálculos novos no Relatório de Integradores

--- src/pcronos/integracao/fornecedor/relatorios/HorasIniJobsFornecedores.java
+++ src/pcronos/integracao/fornecedor/relatorios/HorasIniJobsFornecedores.java
@@ -9,14 +9,20 @@ public class HorasIniJobsFornecedores {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println(Utils.rpad("ID", 4) + " " + Utils.rpad("Fornecedor", 29) + " "
+			    + Utils.rpad("Horários", 35) + " "
 			    + Utils.rpad("Freq.", 6) + " "
 				+ Utils.rpad("Minuto Atual", 13) + " " + Utils.rpad("Minuto v 3.1.0 e maior", 6));
+
 		System.out.println(Utils.rpad("==", 4) + " " + Utils.rpad("==========", 29) + " "
+				+ Utils.rpad("========", 35) + " "
 				+ Utils.rpad("====", 6) + " "
 				+ Utils.rpad("============", 13) + " " + Utils.rpad("======================", 6));
+		
 		System.out.println(Utils.rpad("", 4) + " " + Utils.rpad("", 29) + " "
+				+ Utils.rpad("(min)", 35) + " "
 				+ Utils.rpad("(min)", 6) + " "
 				+ Utils.rpad("(hh:mm:ss)", 13) + " " + Utils.rpad("(hh:mm:ss)", 6));
+
 		System.out.println();
 
 		for (Entry<Integer, Fornecedor> entry : FornecedorRepositorio.hashMap.entrySet()) {
@@ -101,13 +107,21 @@ public class HorasIniJobsFornecedores {
         		segundoAgendamentoAtual = "47";
             }
 
+            String minutos = "";
+            int iFreq = Integer.parseInt(Utils.replaceNull(((Fornecedor) value).FrequenciaProcessamento).replace(" min",""));
+            for (int i = 1; i <= (60 / iFreq); i++)
+            {
+            	minutos += Utils.lpad(Integer.toString(Integer.parseInt(minutoAgendamentoAtual) + i * iFreq), 2, '0') + "-";
+            }
+            minutos = minutos.substring(0, (minutos.length() - 1));
             
 			System.out.println(Utils.rpad(Utils.replaceNull(idFornecedor).toString(), 4) + " "
 					+ Utils.rpad(Utils.replaceNull(((Fornecedor) value).NomeFantasiaEmpresa).replace("ência","."), 29) + " "
+					+ Utils.rpad(minutos, 35) + " "
 					+ Utils.rpad(Utils.lpad(Utils.replaceNull(((Fornecedor) value).FrequenciaProcessamento).replace(" min",""), 3, ' '), 6) + " "
 					+ Utils.rpad(("00:" + Utils.replaceNull(minutoAgendamentoAtual) + ":" + segundoAgendamentoAtual), 13) + " "
 					+ Utils.rpad(("00:" + Utils.replaceNull(minutoAgendamentoFuturo) + ":" + segundoAgendamentoFuturo), 6));
-		}
-	}
+		} // loop sobre os fornecedores
+	} // main()
 
 }
